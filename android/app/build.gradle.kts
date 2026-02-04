7c76f29a60f400a8430f13cb50658715707b8359
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2026-02-04 16:35:41 -0500

    chore: bump version to 0.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.notisync"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildTypes {
